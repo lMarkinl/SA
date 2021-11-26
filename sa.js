0fb4659a2e078ea29745f8bd69393565c1a7a66f
lMarkinl <mphelipe553@gmail.com>
2021-11-26 15:48:02 -0300

    SA

---
+++ sa.js
@@ -0,0 +1,11 @@
+function cadas(){
+    window.location = "Cadastro.html";
+}
+
+function ent(){
+    window.location = "entrar.html";
+}
+
+function aindan(){
+    window.location = "Cadastro.html"
+}
