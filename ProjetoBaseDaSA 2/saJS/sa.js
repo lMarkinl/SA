7268921b666ef9e82abafee75b2406028c9d9f3d
lMarkinl <mphelipe553@gmail.com>
2022-01-04 17:11:18 -0300

    Atualização

---
+++ ProjetoBaseDaSA 2/saJS/sa.js
@@ -0,0 +1,17 @@
+function cadas(){
+    window.location = "Cadastro.html";
+}
+
+function ent(){
+    window.location = "entrar.html";
+}
+
+function aindan(){
+    window.location = "Cadastro.html"
+}
+function home(){
+    window.location = "index.html"
+}
+function sub(){
+    window.location = "sobre.html"
+}
