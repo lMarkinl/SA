9ec4295e88b95cb8a375153a97fa48c38d6a3fa6
lMarkinl <91993583+lMarkinl@users.noreply.github.com>
2022-02-18 19:59:00 -0300

    Add files via upload

---
+++ public/js/index.js
@@ -0,0 +1,10 @@
+$('.navAni').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#link").toggleClass("show_list");
+    $("#link").fadeIn();
+
+});
+
+
+
